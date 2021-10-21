c18d1cf2e31327583aa1377e6fb607340bd5cdf0
ezequielc22 <e_cespedes_c@hotmail.com>
2021-10-20 21:05:11 -0300

    orden de compra generada con id

--- src/components/CartContainer/CartContainer.js
+++ src/components/CartContainer/CartContainer.js
@@ -6,12 +6,44 @@ import "./CartContainer.css";
 
 //externals
 import DeleteIcon from '@mui/icons-material/Delete';
+import { Button } from '@material-ui/core';
 
 //context
 import CartContext from '../../contexts/CartContext'
+import db from '../../firebase';
+import { addDoc, collection } from '@firebase/firestore';
 
 const CartContainer = () => {
     const { products, removeItem, clear } = useContext(CartContext);
+
+    const calcularTotal = () =>{
+        const aux = products.map((prod)=>prod.quantity * parseInt(prod.item.price))
+        const total = aux.reduce((acc,item)=>{ return acc= acc + item;});
+        return total;
+    }
+
+    const newOrder = {
+        buyer:{
+            name: "Marcelo",
+            email: "marcelo_alderete@hotmail.com",
+            phone: "3819098734"
+        },
+        items: products,
+        total: calcularTotal()+" €"
+
+    }
+
+    const addOrder = () =>{
+        console.log("Orden generada: ", newOrder )
+        pushOrderFirebase(newOrder);
+    }
+
+    const pushOrderFirebase = async(newOrder) =>{
+        const orderFirebase = collection(db,'orders');
+        const order = await addDoc(orderFirebase, newOrder)
+        console.log("se genero orden con id: ", order.id)
+
+    }
 
     return (
         <div className="cart-container">
@@ -37,10 +69,13 @@ const CartContainer = () => {
                     );
                 })}
             </ul>
-            <div className="icon-container" style={{ display: "flex", flexDirection: "row", width: "50px", height: "50px", paddingRight: "35px" }}>
-                <button onClick={clear} style={{display: "flex", margin: "auto"}} >
+            <div className="icon-container" style={{ display: "flex", flexDirection: "column", width: "50px", height: "50px", paddingRight: "35px"}}>
+                <Button onClick={clear} style={{display: "flex", margin: "auto", border:"1px solid black"}} >
                     <DeleteIcon sx={{ fontSize: 15 }}/>
-                </button>
+                </Button>
+                <Button onClick={addOrder} variant="outlined" color="secondary" style={{color:"black", border:"1px solid black", marginTop: "40px"}}>
+                    Generar Orden
+                </Button> 
             </div>
         
         </div>
